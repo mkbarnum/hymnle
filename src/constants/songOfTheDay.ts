fd0f65cf557d922bd25c98a2be1c2101bdf8dad7
Matthew Barnum <matbarnu@amazon.com>
2025-02-16 15:18:24 -0800

    more songs

--- src/constants/songOfTheDay.ts
+++ src/constants/songOfTheDay.ts
@@ -51,10 +51,65 @@ export const SONG_OF_THE_DAY = [
     /* Fri Jan 24th */
     {"playFrom": 2000, "number": "78", "title": "I'm Trying to Be like Jesus", "url": "https://www.churchofjesuschrist.org/media/music/songs/im-trying-to-be-like-jesus?crumbs=childrens-songbook&lang=eng", "mp3_url": "https://media2.ldscdn.org/assets/music/childrens-songbook/2002-01-0740-im-trying-to-be-like-jesus-words-and-music-192k-eng.mp3", "book":"CHILDREN'S"},
     /* Sat Jan 25th */
+    {"playFrom": 2000, "number": "92", "title": "The Hearts of the Children", "url": "https://www.churchofjesuschrist.org/media/music/songs/the-hearts-of-the-children?crumbs=childrens-songbook&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/26/c8/26c84c1b45f2e849d5419114468d867a09f930bb/the_hearts_of_the_children_vocal_accompaniment_eng.mp3", "book":"CHILDREN'S"},
     /* Sun Jan 26th */
+    {"number": "291", "title": "Turn Your Hearts", "url": "https://www.churchofjesuschrist.org/media/music/songs/turn-your-hearts?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/a2/2e/a22e1a1c9e0d29747b7b80d97ed261ab58840a84/turn_your_hearts_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
     /* Mon Jan 27th */
+    {"number": "105", "title": "Master, the Tempest Is Raging", "url": "https://www.churchofjesuschrist.org/media/music/songs/master-the-tempest-is-raging?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/ff/d3/ffd3830fbc0e9a4e3349b323b30bdfd45a7eaa68/master_the_tempest_is_raging_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
     /* Tue Jan 28th */
+    {"number": "125", "title": "How Gentle God's Commands", "url": "https://www.churchofjesuschrist.org/media/music/songs/how-gentle-gods-commands?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/37/3a/373a4b0aa6b0198b0572da3950064dde0b426c59/how_gentle_gods_commands_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
     /* Wed Jan 29th */
+    {"number": "335", "title": "Brightly Beams Our Father's Mercy", "url": "https://www.churchofjesuschrist.org/media/music/songs/brightly-beams-our-fathers-mercy-mens-choir?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/e2/f1/e2f1a518d67851716ea95e7b008266df73868a69/brightly_beams_our_fathers_mercy_mens_choir_words_and_music.mp3", "book":"HYMNAL"},
     /* Thu Jan 30th */
+    {"number": "285", "title": "You've Had a Birthday", "url": "https://www.churchofjesuschrist.org/media/music/songs/youve-had-a-birthday?crumbs=childrens-songbook&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/4c/c6/4cc68e9ef043f6a22a4d9c6e15dc56c352797c55/youve_had_a_birthday_round_vocal_accompaniment_eng.mp3", "book":"CHILDREN'S"},
     /* Fri Jan 31st */
+    {"playFrom": 2200, "number": "76", "title": "This Is My Beloved Son", "url": "https://www.churchofjesuschrist.org/media/music/songs/this-is-my-beloved-son?crumbs=childrens-songbook&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/48/d8/48d8e109036d9309e8f86c2788d96ed0d19884d9/this_is_my_beloved_son_vocal_accompaniment_eng.mp3", "book":"CHILDREN'S"},
+    /* Sat Feb 1st */
+    {"number": "249", "title": "Called to Serve", "url": "https://www.churchofjesuschrist.org/media/music/songs/called-to-serve?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/0c/65/0c65fad9c5b3f2cf408bc16b990280defac66673/called_to_serve_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
+    /* Sun Feb 2nd */
+    {"number": "194", "title": "There Is a Green Hill Far Away", "url": "https://www.churchofjesuschrist.org/media/music/songs/there-is-a-green-hill-far-away?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/a1/d7/a1d7f01400ebf8a3fd96f03fbd1a5f2dd1c1bc40/there_is_a_green_hill_far_away_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
+    /* Mon Feb 3rd */
+    {"number": "132", "title": "God Is in His Holy Temple", "url": "https://www.churchofjesuschrist.org/media/music/songs/god-is-in-his-holy-temple?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/41/d4/41d43d4ccc318116474b5943a3765d3d1ed817f5/god_is_in_his_holy_temple_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
+    /* Tue Feb 4th */
+    {"playFrom": 1900, "number": "105", "title": "The Holy Ghost", "url": "https://www.churchofjesuschrist.org/media/music/songs/the-holy-ghost?crumbs=childrens-songbook&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/fe/ff/feff8ff8299844a13226f77a9779d5056fc486e3/the_holy_ghost_vocal_accompaniment_eng.mp3", "book":"CHILDREN'S"},
+    /* Wed Feb 5th */
+    {"number": "129", "title": "Where Can I Turn for Peace?", "url": "https://www.churchofjesuschrist.org/media/music/songs/where-can-i-turn-for-peace?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/b4/ad/b4ad269004c73e5ccc0d0c933e7bd79da6404060/where_can_i_turn_for_peace_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
+    /* Thu Feb 6th */
+    {"number": "308", "title": "Love One Another", "url": "https://www.churchofjesuschrist.org/media/music/songs/love-one-another?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/65/30/653050322421f2a9d79d2e4ad8ff7e8b25303e91/love_one_another_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
+    /* Fri Feb 7th */
+    {"number": "109", "title": "Search, Ponder, and Pray", "url": "https://www.churchofjesuschrist.org/media/music/songs/search-ponder-and-pray?crumbs=childrens-songbook&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/be/85/be851a9d6e402cd46773525792bc404ab70219a3/search_ponder_and_pray_vocal_accompaniment_eng.mp3", "book":"CHILDREN'S"},
+    /* Sat Feb 8th */
+    {"number": "58", "title": "Come, Ye Children of the Lord", "url": "https://www.churchofjesuschrist.org/media/music/songs/come-ye-children-of-the-lord?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/3a/77/3a77a13b227d1f72ad9ec3a9cc6e921e8e2285c7/come_ye_children_of_the_lord_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
+    /* Sun Feb 9th */
+    {"number": "136", "title": "I Know That My Redeemer Lives", "url": "https://www.churchofjesuschrist.org/media/music/songs/i-know-that-my-redeemer-lives?crumbs=hymns&lang=eng", "mp3_url": "https://media2.ldscdn.org/assets/music/hymns/2001-01-1360-i-know-that-my-redeemer-lives-vocal-and-instrumental-192k-eng.mp3", "book":"HYMNAL"},
+    /* Mon Feb 10th */
+    {"number": "142", "title": "Sweet Hour of Prayer", "url": "https://www.churchofjesuschrist.org/media/music/songs/sweet-hour-of-prayer?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/1a/f7/1af762f06655c9af2859db7894ebe003d9e1fe8a/sweet_hour_of_prayer_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
+    /* Tue Feb 11th */
+    {"number": "120", "title": "Nephi's Courage", "url": "https://www.churchofjesuschrist.org/media/music/songs/nephis-courage?crumbs=childrens-songbook&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/4d/25/4d258204b4653c0f3fd668a7c8c9d10b7d8ca200/nephis_courage_vocal_accompaniment_eng.mp3", "book":"CHILDREN'S"},
+    /* Wed Feb 12th */    
+    {"number": "223", "title": "Have I Done Any Good?", "url": "https://www.churchofjesuschrist.org/media/music/songs/have-i-done-any-good?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/60/0f/600f94a8c4300f9094b4db8adf817f6e65feafba/have_i_done_any_good_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
+    /* Thu Feb 13th */
+    {"number": "97", "title": "Lead, Kindly Light", "url": "https://www.churchofjesuschrist.org/media/music/songs/lead-kindly-light?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/3f/49/3f49bc95df1cb23f571430b9fc009efeeccecc12/lead_kindly_light_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
+    /* Fri Feb 14th */
+    {"number": "294", "title": "Love at Home", "url": "https://www.churchofjesuschrist.org/media/music/songs/love-at-home?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/7a/24/7a24b2e15613f5ce1ef0fcbb68825f78294542dd/love_at_home_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
+    /* Sat Feb 15th */
+    {"number": "138", "title": "Where Love Is", "url": "https://www.churchofjesuschrist.org/media/music/songs/where-love-is?crumbs=childrens-songbook&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/dd/a2/dda2bdec68b1ff30eb71bcd66643c0b15f43bda4/where_love_is_vocal_accompaniment_eng.mp3", "book":"CHILDREN'S"},
+    /* Sun Feb 16th */
+    {"number": "113", "title": "Our Savior's Love", "url": "https://www.churchofjesuschrist.org/media/music/songs/our-saviors-love?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/bd/a2/bda2c9a611ae23b4ab2732a548ab8ec641fecc7b/our_saviors_love_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
+    /* Mon Feb 17th */
+    {"number": "338", "title": "America the Beautiful", "url": "https://www.churchofjesuschrist.org/media/music/songs/america-the-beautiful?crumbs=hymns&lang=eng", "mp3_url": "https://assets.churchofjesuschrist.org/be/c8/bec8726488f612c3a9ffb45435ec7f81d051dd46/america_the_beautiful_vocal_accompaniment_eng.mp3", "book":"HYMNAL"},
+    /* Tue Feb 18th */
+    /* Wed Feb 19th */
+    /* Thu Feb 20th */
+    /* Fri Feb 21st */
+    /* Sat Feb 22nd */
+    /* Sun Feb 23rd */
+    /* Mon Feb 24th */
+    /* Tue Feb 25th */
+    /* Wed Feb 26th */
+    /* Thu Feb 27th */
+    /* Fri Feb 28th */
+    /* Sat Mar 1st */
+    /* Sun Mar 2nd */
+    /* Mon Mar 3rd */
 ]
